e8e0fd6e838c3cb1ca5e394ef6fc3e9004b7087b
abdul-almusaileem <7amanga@gmail.com>
2019-05-21 16:01:21 -0400

    still working on movement, didn't experement more with diff functions

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from time import sleep
 
 # define the pins for each servoe
 #
-PIN_1 = machine.Pin(5)
-PIN_2 = machine.Pin(18)
+PIN_1 = machine.Pin(18)
+PIN_2 = machine.Pin(5)
 PIN_3 = machine.Pin(17)
 
 # create
@@ -25,7 +25,6 @@ SERVOS = [SERVO_1, SERVO_2, SERVO_3]
 #
 #
 def main():
-    while True:
         move()
 
 
@@ -34,13 +33,30 @@ def move():
     # move the
     #
     #while True:
+        # the plus 20 is for the caliprating the head
+        #
     SERVOS[0].duty(96)
-    SERVOS[1].duty(77)
-    SERVOS[2].duty(59+20)
+    SERVOS[1].duty(96)
+    SERVOS[2].duty(96)
     sleep(1)
-    SERVOS[0].duty(59)
-    SERVOS[1].duty(77)
-    SERVOS[2].duty(96+20)
+
+        #straigh()
+
+
+
+        #straigh()
+
+def move2():
+    SERVOS[0].duty(50)
+    SERVOS[1].duty(40)
+    SERVOS[2].duty(40)
+    sleep(1)
+
+
+def straigh():
+    SERVOS[0].duty(77+20)
+    SERVOS[1].duty(77-20)
+    SERVOS[2].duty(77)
     sleep(1)
 
 
